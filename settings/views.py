087e5e0c5e1b3f4435c5a03a7837b091253af065
Mmy2000 <yahianada005@gmail.com>
2024-07-17 23:56:57 +0300

    starting with models

--- settings/views.py
+++ settings/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from service.models import Services
+from products.models import Blinds , Curtains
 # Create your views here.
 def home(request):
-    return render(request , 'home.html' )
+    services = Services.objects.all()[:4]
+    blinds = Blinds.objects.all()[:3]
+    curtains = Curtains.objects.all()[:3]
+    context = {
+        'services':services,
+        'blinds':blinds,
+        'curtains':curtains
+    }
+    return render(request , 'home.html' , context )
